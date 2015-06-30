d6739aef00ee8c92ffc8d54dad15721410263e16
ventaquil <konrad.golawski@gmail.com>
2015-06-30 15:17:16 +0200

    Initial commit
    I sent class in PHP and README file.

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Dijkstra-s-algorithm
+# Dijkstra's algorithm
+
+How to use:
+
+```php
+    <?php
+    use algorithms;
+
+    // Simple relations
+    $relations=[
+      1=>[
+        [2,1],
+        [4,2],
+      ], # 1
+      2=>[
+        [1,1],
+        [3,3],
+        [4,3],
+      ], # 2
+      3=>[
+        [2,3],
+        [4,1],
+        [5,5],
+      ], # 3
+      4=>[
+        [1,2],
+        [2,3],
+        [3,1],
+        [5,1],
+      ], # 4
+      5=>[
+        [3,5],
+        [4,1],
+      ], # 5
+    ]; # $relations
+
+    $dijkstra=new dijkstra;
+    $dijkstra->setRelations($relations);
+    $generated=$dijkstra->generate();
+```
---
+++ dijkstra.php
@@ -0,0 +1,93 @@
+<?php
+namespace algorithms;
+
+class dijkstra {
+    protected $points;
+    protected $relations;
+
+    /*
+     * @arg: (array) relations array
+     * @desc: Method put relations to $relations protected variable.
+     */
+    public function setRelations($relations_array){
+        if(is_array($relations)){
+            $this->relations=$relations_array;
+        } # if()
+    } # setRelations()
+
+    /*
+     * @desc: Method analyze all relations from $relations variable.
+     */
+    public function generate(){
+        $result=[]; # Prepare results array
+
+        # Analyze all relations
+            foreach($this->relations as $point=>$relation){
+                # Prepare $points array by isset source point
+                    $this->points=[
+                        $point=>[
+                            0,
+                            '',
+                        ], # []
+                    ]; # $this->points
+
+                $this->d($point,$point);
+                $result[$point]=$this->points; # Copy $points content to results array
+            } # foreach()
+
+        return $result;
+    } # generate()
+
+    /*
+     * @arg: (int) source point
+     * @arg: (int) already analised point
+     * @arg^: (array) already visited points
+     * @desc: Method analyzes current point, point neighborhood and go by minimum way to unvisited point.
+     */
+    public function d($source,$point,&$visited=[]){
+        $visited[$point]=TRUE; # Set current point as visited
+
+        # Prepare help variables
+            $min_ptr=-1;
+            $min=0;
+
+        # Analyzes point neighborhood
+            foreach($this->relations[$point] as $relation){
+                if($relation[0]!=$source){ # If current point is different than source
+                    if(empty($visited[$relation[0]])){ # If current point is not visited
+                        if($min_ptr==-1){ # When minimal point is not finded
+                            $min_ptr=$relation[0];
+                            $min=$relation[1];
+                        } # if()
+                        else{
+                            if($min>$relation[1]){
+                                $min_ptr=$relation[0];
+                                $min=$relation[1];
+                            } # if()
+                        } # else
+                    } # if()
+
+                    # Change the shortest way to current point
+                        if(empty($this->points[$relation[0]])){
+                            $this->points[$relation[0]]=[
+                                $this->points[$point][0]+$relation[1],
+                                ((empty($this->points[$point][1]))?$point:$this->points[$point][1]).':'.$relation[0],
+                            ]; # $this->points[]
+                        } # if()
+                        else{
+                            if($this->points[$relation[0]][0]>$this->points[$point][0]+$relation[1]){
+                                $this->points[$relation[0]]=[
+                                    $this->points[$point][0]+$relation[1],
+                                    ((empty($this->points[$point][1]))?NULL:$this->points[$point][1].':').$relation[0],
+                                ]; # $this->points[]
+                            } # if()
+                        } # else
+                } # if()
+            } # foreach()
+
+        # If isset unvisited point with minimal way go for it
+            if($min_ptr!=-1){
+                $this->d($source,$min_ptr,$visited);
+            } # if()
+    } # d()
+} # dijkstra
