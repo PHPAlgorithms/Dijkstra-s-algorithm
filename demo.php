7762061e72a7e5193bf9b5836b7c56f59d81ff4c
Konrad Goławski <konrad.golawski@gmail.com>
2015-07-28 13:00:38 +0200

    Not connectivity graph test
    Working test in demo.php file.

--- demo.php
+++ demo.php
@@ -32,3 +32,25 @@
     $dijkstra = new algorithms\dijkstra($relations);
 	var_dump($dijkstra->distances(1)); // all distances from point 1 to other points
     var_dump($dijkstra->generate()); // all distances from all points to all points
+	
+	$relations = [
+		1 => [ // point 1 relations
+			[2, 1], // to point 2 - distance 1
+		],
+		2 => [ // point 2 relations
+			[3, 1], // to point 3 - distance 1
+		],
+		3 => [ // point 3 relations
+			[1, 1], // to point 1 - distance 1
+		],
+		4 => [ // point 4 relations
+			[5, 1], // to point 5 - distance 1
+		],
+		5 => [ // point 5 relations
+			[4, 1], // to point 5 - distance 1
+		],
+	];
+
+    $dijkstra = new algorithms\dijkstra($relations);
+	var_dump($dijkstra->distances(1)); // all distances from point 1 to other points
+    var_dump($dijkstra->generate()); // all distances from all points to all points
